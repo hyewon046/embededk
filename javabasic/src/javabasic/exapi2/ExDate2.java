f293630f0d3c3e7baa5c3ff5ead57a6c40642cf3
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-11-14 14:44:21 +0900

    406

--- javabasic/src/javabasic/exapi2/ExDate2.java
+++ javabasic/src/javabasic/exapi2/ExDate2.java
@@ -1,8 +1,21 @@
 package javabasic.exapi2;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class ExDate2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("연도와 월을 입력해주세요");
+		int year = sc.nextInt();
+		int month = sc.nextInt();
+		System.out.println("연도 : " + year + ", 월 :"+  month);
+		
+		Calendar cal = Calendar.getInstance();
+		
+				
+		
 
 	}
 

6ccc6b992095d0dff07270d484df3dbcfa708ead
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-11-20 17:49:37 +0900

    406

--- javabasic/src/javabasic/excollection/Player.java
+++ javabasic/src/javabasic/excollection/Player.java
@@ -1,11 +1,14 @@
 package javabasic.excollection;
 
 import java.util.List;
+import java.util.Map;
 
 public class Player {
 	
 	private String name; //이름
 	private List<Card> cardList; //카드리스트
+	private Map<Integer, Integer> numCountMap; //숫자카운트맵
+	private Map<String, Integer> shapeCountMap; //무늬카운트맵
 	
 	public Player() {
 	}
@@ -32,9 +35,25 @@ public class Player {
 		this.cardList = cardList;
 	}
 	
+	public Map<Integer, Integer> getNumCountMap() {
+		return numCountMap;
+	}
+
+	public void setNumCountMap(Map<Integer, Integer> numCountMap) {
+		this.numCountMap = numCountMap;
+	}
+
+	public Map<String, Integer> getShapeCountMap() {
+		return shapeCountMap;
+	}
+
+	public void setShapeCountMap(Map<String, Integer> shapeCountMap) {
+		this.shapeCountMap = shapeCountMap;
+	}
+
 	@Override
 	public String toString() {
-		return name + " " + cardList;
+		return name + " " + cardList + "\n" + numCountMap + "\n" + shapeCountMap;
 	}
 
 }
--- javabasic/src/javabasic/excollection/PokerMain.java
+++ javabasic/src/javabasic/excollection/PokerMain.java
@@ -1,8 +1,10 @@
-package javabasic.excollection;
+package javabasic.excollection; 
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PokerMain {
 
@@ -28,25 +30,70 @@ public class PokerMain {
 				totalCardList.add(new Card(j, shapeList.get(i)));
 			}
 		}
-		System.out.println(totalCardList);
+		//System.out.println(totalCardList);
 		
 		//카드 섞기
 		Collections.shuffle(totalCardList);
-		System.out.println(totalCardList);
+		//System.out.println(totalCardList);
 		
 		//플레이어들에게 7장씩 나눠준다
 		//List 인터페이스에 List<E> subList(int~) 메소드
 		for (int i=0; i<playerList.size(); i++) {
-			playerList.get(i).setCardList(totalCardList.subList(i*7, (i*7)+7)); //player가 카드리스트를 가지고있으니 셋을 함
+			playerList.get(i).setCardList(totalCardList.subList(i*7, (i*7)+7)); //player가 카드리스트를 가지고있으니 셋으로 불러옴
 		}
+		
+		//플레이어의 숫자카운트맵, 무늬카운트맵을 초기화하고 값을 설정
+		for (int i=0; i<playerList.size(); i++) {
+			
+			//플레이어의 숫자카운트맵, 무늬카운트맵을 생성
+			Player player = playerList.get(i);
+			player.setNumCountMap(new HashMap<Integer, Integer>());
+			player.setShapeCountMap(new HashMap<String, Integer>());
+			
+			//플레이어의 숫자카운트맵, 무늬카운트맵을 초기화
+			initMap(player.getNumCountMap(), player.getShapeCountMap());
+			
+			//카드의 숫자, 무늬 카운팅
+			for (Card card : player.getCardList()) {
+				player.getNumCountMap().put(card.getNumber(), 
+						player.getNumCountMap().get(card.getNumber())+1);
+				player.getShapeCountMap().put(card.getShape(), 
+						player.getShapeCountMap().get(card.getShape())+1);
+			}
+		}
+		
 		for (Player player : playerList) {
 			System.out.println(player);
+			printResult(player);
 		}
-		
-		
-		
-		
-	
+
+
 	}//main
+	
+	//플레이어의 숫자카운트맵, 무늬카운트맵을 초기화하는 메소드
+	private static void initMap(Map<Integer, Integer> numCountMap, 
+			Map<String, Integer> shapeCountMap) {
+			for (int i =2; i<15; i++) {
+				numCountMap.put(i, 0);
+			}
+			shapeCountMap.put("S", 0);
+			shapeCountMap.put("H", 0);
+			shapeCountMap.put("D", 0);
+			shapeCountMap.put("C", 0);
+	}
+	
+	private static void printResult(Player player) {
+		String resultStr = "";
+		if (PokerRule.isRoyalStraightFlush(player)) resultStr = "[로열스트레이트플러쉬]";
+		else if (PokerRule.isStraightFlush(player)) resultStr = "[스트레이트플러쉬]";
+		else if (PokerRule.isFourCard(player)) resultStr = "[포카드]";
+		else if (PokerRule.isFullHouse(player)) resultStr = "[풀하우스]";
+		else if (PokerRule.isFlush(player)) resultStr = "[플러쉬]";
+		else if (PokerRule.isStraight(player)) resultStr = "[스트레이트]";
+		else if (PokerRule.isTriple(player)) resultStr = "[트리플]";
+		else if (PokerRule.isTwoPair(player)) resultStr = "[투 페어]";
+		else if (PokerRule.isOnePair(player)) resultStr = "[원 페어]";
+		else resultStr = "[족보 없음]" +", 하이카드 : " + PokerRule.highCard(player); //하이카드가 10이하일때 숫자로 나오고 있는지 해결을 못함
+	}
 
 }//class
